ac53869eda0cdd9404b0ea6d0a912e659b815612
elzaziedina <ziedina.elza@gmail.com>
2022-02-24 14:52:38 +0200

    Otrais

--- script.js
+++ script.js
@@ -13,3 +13,34 @@ function nolasa(m1, m2, m3){
         return false;
     }
 }
+
+function perimetrs(m1, m2, m3) { // izrēķina trijstūra 3 malu summu jeb perimetru
+    const m = nolasa();
+    m1 = m.m1; m2 = m.m2; m3 = m.m3;
+    console.log(m1, m2, m3);
+    const p = m1 + m2 + m3;
+    console.log(p);
+    return(p);
+}
+
+function pusperimetrs(m1, m2, m3) { // izrēķina trijstūra perimetru
+    const pusperimetrs = perimetrs(m1, m2, m3)/2;
+    console.log(pusperimetrs);
+    return(pusperimetrs);
+}
+
+function laukums(m1, m2, m3) { // izv
+    const pusperimetrs = perimetrs(m1, m2, m3)/2;
+    let trijlaukums = Math.sqrt(
+        pusperimetrs*((pusperimetrs - m1)*(pusperimetrs - m2)*(pusperimetrs - m3)));
+        console.log(trijlaukums);
+        return trijlaukums;
+}
+
+function irTrijsturis(m1, m2, m3) { // pārbauda, vai trijstūris eksistē
+    if (m1 < m2 + m3 && m2 < m1 + m3 && m3 < m1 + m2) { // pārbauda, vai izpildās trijstūra nevienādība
+        return true; // atgriež true
+    } else {
+        return false; // atgriež false
+    }
+}
